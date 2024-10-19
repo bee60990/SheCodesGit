723c2e26db37b0413e296cb53892eb279b038214
bee60990 <155386705+bee60990@users.noreply.github.com>
2024-10-19 10:51:31 +0200

    Changed Code

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function showResponse(response) {
+  let tempElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.temperature.current);
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = response.data.city;
+  tempElement.innerHTML = temperature;
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
+  let city = searchInputElement.value;
+
+  let apiKey = "9530ba64d5fta9c45b62cdbab1131eoe";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showResponse);
 }
 
 function formatDate(date) {
