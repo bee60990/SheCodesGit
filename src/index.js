9926442f31baa3f52dfe9c10a925efc44e642181
bee60990 <155386705+bee60990@users.noreply.github.com>
2024-10-19 10:47:03 +0200

    Changed code

--- src/index.js
+++ src/index.js
@@ -1,59 +1,41 @@
-//In your project, display the current date and time using JavaScript: Tuesday 16:00
-
-let currentTime = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let day = days[currentTime.getDay()];
-
-let hour = currentTime.getHours();
-let min = currentTime.getMinutes();
-let date = `${day} ${hour}:${min}`;
-
-let rightDate = document.querySelector(".current-details span");
-rightDate.innerHTML = date;
-
-//search bar, change h1
-
-function showCity(event) {
+function search(event) {
   event.preventDefault();
-  let city = document.querySelector(".search-input");
-  let heading = document.querySelector(".current-city");
-  heading.innerHTML = city.value;
+  let searchInputElement = document.querySelector("#search-input");
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = searchInputElement.value;
 }
 
-let searchBar = document.querySelector("form");
-searchBar.addEventListener("submit", showCity);
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
 
-function displayResponse(response) {
-  console.log(response);
-  console.log(response.data[0].email);
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
 }
 
-let apiUrl = "https://jsonplaceholder.typicode.com/comments";
-axios.get(apiUrl).then(displayResponse);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
 
-function showResponse(response) {
-  console.log(response.data.temperature.current);
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
 
-  let tempElement = document.querySelector("#temperature");
-  let temperature = Math.round(response.data.temperature.current);
-
-  let city = "Sydney";
-  let description = response.data.condition.description;
-  tempElement.innerHTML = `It is ${temperature}°C in ${city}`;
-}
-
-let apiKey = "9530ba64d5fta9c45b62cdbab1131eoe";
-
-let apiUrl =
-  "https://api.shecodes.io/weather/v1/current?query=${city}&key=9530ba64d5fta9c45b62cdbab1131eoe&units=metric";
-
-axios.get(apiUrl).then(showResponse);
+currentDateELement.innerHTML = formatDate(currentDate);
